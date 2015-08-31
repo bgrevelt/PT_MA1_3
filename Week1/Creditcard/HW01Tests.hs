4dea14a2b78f9d8d4cc5062af2544d7b93073a1c
Futsy <florisvelleman@hotmail.com>
2015-08-31 16:57:43 +0200

    PT_MA1_3: Week1: Creditcard: Added first three exercises, including test cases.

---
+++ Week1/Creditcard/HW01Tests.hs
@@ -0,0 +1,78 @@
+-- CIS 194, Spring 2015
+--
+-- Test cases for HW 01
+
+module HW01Tests where
+
+import HW01
+import Testing
+
+-- Exercise 1 -----------------------------------------
+
+testLastDigit :: (Integer, Integer) -> Bool
+testLastDigit (n, d) = lastDigit n == d
+
+testDropLastDigit :: (Integer, Integer) -> Bool
+testDropLastDigit (n, d) = dropLastDigit n == d
+
+ex1Tests :: [Test]
+ex1Tests = [ Test "lastDigit test" testLastDigit
+             [(123, 3), (1234, 4), (5, 5), (10, 0), (0, 0)]
+           , Test "dropLastDigit test" testDropLastDigit
+             [(123, 12), (1234, 123), (5, 0), (10, 1), (0,0)]
+           ]
+
+-- Exercise 2 -----------------------------------------
+
+testToRevDigits:: (Integer, [Integer]) -> Bool
+testToRevDigits (n, d) = toRevDigits n == d
+
+ex2Tests :: [Test]
+ex2Tests = [ Test "toRevDigits test" testToRevDigits
+             [(123, [3, 2, 1]), (0, []), (-1, []), (5, [5]), 
+               (3219873218, [8, 1, 2, 3, 7, 8, 9, 1, 2, 3])
+             ]
+           ]
+
+-- Exercise 3 -----------------------------------------
+
+testDoubleEveryOther:: ([Integer], [Integer]) -> Bool
+testDoubleEveryOther (n, d) = doubleEveryOther n == d
+
+ex3Tests :: [Test]
+ex3Tests = [ Test "doubleEveryOther test" testDoubleEveryOther
+             [([1, 2, 3, 4], [1, 4, 3, 8]), ([4, 9, 5, 5], [4, 18, 5, 10]),
+              ([1, 2, 3], [1, 4, 3]), ([], []), ([1], [1])
+             ]
+           ]
+
+-- Exercise 4 -----------------------------------------
+
+ex4Tests :: [Test]
+ex4Tests = []
+
+-- Exercise 5 -----------------------------------------
+
+ex5Tests :: [Test]
+ex5Tests = []
+
+-- Exercise 6 -----------------------------------------
+
+ex6Tests :: [Test]
+ex6Tests = []
+
+-- All Tests ------------------------------------------
+
+allTests :: [Test]
+allTests = concat [ ex1Tests
+                  , ex2Tests
+                  , ex3Tests
+                  , ex4Tests
+                  , ex5Tests
+                  , ex6Tests
+                  ]
+
+-- Run all the tests ----------------------------------
+
+runAllTests :: [Failure]
+runAllTests = runTests allTests
