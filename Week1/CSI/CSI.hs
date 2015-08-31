4fa3ec8ce99ae46605920c0ce72c1782dc98c029
Alex van Manen <alexvanmanen@hotmail.com>
2015-09-01 00:42:54 +0200

    Work in progres.. Filtering set

--- Week1/CSI/CSI.hs
+++ Week1/CSI/CSI.hs
@@ -6,24 +6,18 @@ boys = [Matthew, Peter, Jack, Arnold, Carl]
 
 
 says :: Boy -> Boy -> Bool
---says Matthew Matthew :: False
---says Matthew Carl :: False
---says Peter Matthew :: True
 says p1 p2 | p1==Jack && p2 == Matthew = False
            | p1==Jack && p2 == Peter = False
            | p1==Carl && p2 == Arnold = False
            | p1==Arnold && (p2 == Peter || p2 == Matthew) = False
-            | otherwise = True
-            
-
+           | otherwise = True
  
-
 accusers :: Boy -> [Boy]
 accusers Peter = [Matthew, Jack]
 accusers Matthew = [Peter, Jack, Arnold]
-accusers Jack =   boys
-accusers Carl = boys
-accusers Arnold = boys
+accusers Jack = []
+accusers Carl = []
+accusers Arnold = []
 
 
 guilty, honest :: [Boy]
@@ -31,9 +25,43 @@ guilty = []
 honest = []
 
 
-a = [[True, True, True, True, True], [True, True, True, True, False], [True, True, True, False, False]]
+powerset = foldr (\x acc -> acc ++ map (x:) acc) [[]]
+
+-- all possible solutions
+options = powerset boys
+
+-- filter on three people speak the truth
+only3 = filter(\x -> length x == 3) options
 
 
+
+
+
+
+
+-- Below this is my playground
 b = accusers Matthew
 c = accusers Peter
 
+
+
+
+genIntersect :: Eq a => [[a]] -> [a]
+genIntersect [e] = e
+genIntersect (e:l) = intersect (genIntersect l) e 
+
+
+
+
+
+--als Peter de waarheid spreekt, dan
+--spreekt Jack en Matthew niet de waarheid
+
+hoi :: Boy -> [Boy]
+hoi b = hoi' boys b
+
+hoi'  :: [Boy] -> Boy -> [Boy]
+hoi' [] b2 = []
+hoi' (b:bs) b2 = if (elem b2 (filter (says b2) bs)) 
+                 then [b] ++ hoi' bs b2 
+                 else hoi' bs b2
