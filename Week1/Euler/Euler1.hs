a176a328b19e2a2fc4ff06047e92dd0dc68da97c
jacksierkstra <jacksierkstra@gmail.com>
2015-09-03 11:34:40 +0200

    I had another solution. Just added it for the sake of comparing solutions.

--- Week1/Euler/Euler1.hs
+++ Week1/Euler/Euler1.hs
@@ -1,3 +1,6 @@
 module Euler1 where
 solution :: Integer
 solution = sum [number| number <- [1..1000], rem number 3 ==0 || rem number 5 ==0]
+
+anotherSolution :: Integer
+anotherSolution = sum (filter (\n -> n `mod` 5 == 0 || n `mod` 3 == 0) [1..999])
