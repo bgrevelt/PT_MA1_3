5162469db2d2df23322a708ce43ee93f9ed63ed9
Alex van Manen <alexvanmanen@hotmail.com>
2015-09-12 18:14:58 +0200

    Triangles.hs working with test cases.

--- Week2/Triangles.hs
+++ Week2/Triangles.hs
@@ -3,7 +3,17 @@ module Triangles where
 import Data.List 
 import System.Random
 
+{-
+Concise report
+-----------------------
+Time spent:
+Less than 1 hour creating triangle function
+About 9 hours creating test functions. Why so long? It's difficult if you want to generate random tests and you don't know anything about monads and IO.
 
+Tests testing every different shape that's defined in data Shape with 100 random inputs. 
+
+What can still be improved. Some tests are still weak and don't test every possibility.
+-}
 
 data Shape = NoTriangle | Equilateral 
             | Isosceles  | Rectangular | Other deriving (Eq,Show)
@@ -12,6 +22,7 @@ data Shape = NoTriangle | Equilateral
 triangleMap :: [Integer] -> Shape
 triangleMap (a:b:c:xs) = triangle a b c
 
+
 triangle :: Integer -> Integer -> Integer -> Shape
 triangle a b c | a + b <= c || a + c <= b || b + c <= a = NoTriangle -- geen driehoek
                | a == b && b == c = Equilateral --Gelijkzijdig
@@ -19,36 +30,36 @@ triangle a b c | a + b <= c || a + c <= b || b + c <= a = NoTriangle -- geen dri
                | a == b || a == c || b == c = Isosceles -- Gelijkbenig
                | otherwise = Other -- Anders (wel een driehoek)
 
-getRandGzd :: IO (Integer, Integer, Integer)
-getRandGzd = do 
-  side <- getRandomInt 50
-  return (toInteger side,toInteger side,toInteger side)
 
 
 getRandIsosceles :: IO [Integer]
 getRandIsosceles = do 
-  side <- getRandomInt 50
-  otherSide <- getRandomInt 50
-  return [toInteger side,toInteger side,toInteger otherSide]
+  side <- getRandomInt2 50 100
+  otherSide <- getRandomInt2 50 100
+  return [x | x <- [toInteger side,toInteger side,toInteger side +1]]
 
 getRandNoTriangle :: IO [Integer]
 getRandNoTriangle = do 
   sideA <- getRandomInt 50
   sideB <- getRandomInt 50
-  sideC <- sideA + sideB
-  return [toInteger sideA,toInteger sideB,toInteger sideC]
+  sideC <- getRandomInt 50
+  return [x | x <- [toInteger sideA,toInteger sideB,0]]
 
 getRandRectangular :: IO [Integer]
 getRandRectangular = do 
-  side <- getRandomInt 50
-  otherSide <- getRandomInt 50
-  return [toInteger side,toInteger side,toInteger otherSide]
+  sideA <- getRandomInt2 3 3
+  sideB <- getRandomInt2 4 4
+  sideC <- getRandomInt2 5 5
+  return [toInteger sideA,toInteger sideB, toInteger sideC]
+
+
 
 getRandOther :: IO [Integer]
 getRandOther = do 
-  side <- getRandomInt 50
-  otherSide <- getRandomInt 50
-  return [toInteger side,toInteger side,toInteger otherSide]
+  sideA <- getRandomInt2 51 51
+  sideB <- getRandomInt2 56 56
+  sideC <- getRandomInt2 8 8
+  return [toInteger sideA,toInteger sideB,toInteger sideC]
 
 
 
@@ -60,6 +71,9 @@ getRandEquilateral = do
 
 getRandomInt :: Int -> IO Int
 getRandomInt n = getStdRandom (randomR (1,n))
+
+getRandomInt2  :: Int -> Int -> IO Int
+getRandomInt2 a b = getStdRandom (randomR (a,b))
     
 
 testRT :: Integer -> Integer -> IO [Integer] -> ([Integer] -> Shape)
@@ -97,20 +111,3 @@ test_prop_isosceles =  testPostTriangle getRandIsosceles triangleMap prop_isosce
 test_prop_notriangle =  testPostTriangle getRandNoTriangle triangleMap prop_notriangle
 test_prop_rectangular =  testPostTriangle getRandRectangular triangleMap prop_rectangular
 test_prop_triangle =  testPostTriangle getRandOther triangleMap prop_triangle
-
-
-
-
-
-test2 = triangle 5 5 5 == Equilateral
-test3 = triangle 5 5 6 == Isosceles
-
-
-
-
-
-
-
-
-
-
