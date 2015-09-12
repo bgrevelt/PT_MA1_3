f3b89bde06fccba6ec6e0f9b3cd2bafe9fad384d
jacksierkstra <jacksierkstra@gmail.com>
2015-09-12 09:03:33 +0200

    Changed isPermutation because it had an unmtached pattern. Also imported Data.List because delete wasn't known to the compiler.

--- Week2/Permutations.hs
+++ Week2/Permutations.hs
@@ -1,14 +1,10 @@
 {-# OPTIONS_GHC -Wall #-}
 module Permutations where
 
+import Data.List
+
 isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation [] [] = True                      --- Empty lists are always the same
+isPermutation [] [] = True
 isPermutation xs (y:ys) | length xs /= length (y:ys) = False
                         | otherwise = isPermutation (delete y xs) ys
-
---- If the legth is not the same, it has no purpose in checking further.
---- If listsizes are the same, proceed in deleting the first occurence
---- of an element of the second list from the first list. Keep doing this
---- until the list is empty.
-
---- The next step is generating testdata... Will be continued...
+isPermutation _ _ = False
