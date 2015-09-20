1806b840de14f45ada9d9a41e612fd12c8b55919
jacksierkstra <jacksierkstra@gmail.com>
2015-09-20 15:56:13 +0200

    Changed ParseTests according to the meeting of today.

--- Week3/ParseTest.hs
+++ Week3/ParseTest.hs
@@ -9,9 +9,9 @@ Test report:
   Test method: using predefined in- and output and test if the expected output matches with the output from the parse method. Reason for predefined testing is that with automated generation of test cases it's very difficult to know if you test all the different scenarios. Another reason for not automatically testing is that swapping some parts of an valid expressions to make them invalid may accidentally lead to valid cases. In such a scenario your test doesn't give back the correct result.
 
 
-Outcome of the tests by executing "testParse xs compareForm": 
-  9 of the 15 tests failed. I think the most of the test fall because the specifications are unclear. What should return the expression '-1'. I assume '-1' and not '[-1]'. Or the specifications are wrong or the code. Below are all results of the tests. 
-  
+Outcome of the tests by executing "testParse xs compareForm":
+  9 of the 15 tests failed. I think the most of the test fall because the specifications are unclear. What should return the expression '-1'. I assume '-1' and not '[-1]'. Or the specifications are wrong or the code. Below are all results of the tests.
+
 
 testParse xs compareForm"
 Test failed when parsing: '1'. Expected result: '1', but got: '[1]' "
@@ -34,38 +34,61 @@ Test failed when parsing: '1'. Expected result: '1', but got: '[1]' "
   Time spent: approximately 6 hours
 -}
 
+{-
+
+"Test passed on: '\"1\"'"
+"Test passed on: '\"-2\"'"
+"Test passed on: '\"\"'"
+"Test passed on: '\"*\"'"
+"Test passed on: '\"+\"'"
+"Test passed on: '\"==>\"'"
+"Test passed on: '\"(1==>3)\"'"
+"Test passed on: '\"(1<=>3)\"'"
+"Test passed on: '\"*(1 +(2 -3))\"'"
+"Test passed on: '\"*(1 +(2 -3)\"'"
+"Test passed on: '\" *  (  1 + ( 2 - 3 ) )\"'"
+"Test passed on: '\"(*(2 -3)<=>-*(9 3))\"'"
+"Test passed on: '\"(*(2 -3)==>-+(9 3))\"'"
+"Done"
+
+After the meeting on sunday the 20th, we concluded that the expected results were different
+from what the method specification said it would. Therefore we changed the expected results
+to include brackets, because that is what the method's output is.
+
+-}
+
 
 xs = [
       -- testing single value
-      ("1",                       "[1]"), 
+      ("1",                       "[1]"),
       -- testing negation
-      ("-2",                      "[-2]"), 
+      ("-2",                      "[-2]"),
       -- testing empty form
-      ("",                        "[]"), 
+      ("",                        "[]"),
       -- testing conjunction without values
-      ("*",                       "[]"), 
+      ("*",                       "[]"),
       -- testing disjunction without values
-      ("+",                       "[]"), 
+      ("+",                       "[]"),
       -- testing implication without values
-      ("==>",                     "[]"), 
+      ("==>",                     "[]"),
       -- testing implication without parenthesis
-      -- ("1==>3",                   "1==>3"), 
+      -- ("1==>3",                   "1==>3"),
       -- testing implication with parenthesis
-      ("(1==>3)",                 "[(1==>3)]"), 
+      ("(1==>3)",                 "[(1==>3)]"),
       -- testing  equivalence without parenthesis
-      -- ("1<=>3",                  "1<=>3"), 
+      -- ("1<=>3",                  "1<=>3"),
       -- testing  equivalence with parenthesis
-      ("(1<=>3)",                "[(1<=>3)]"), 
+      ("(1<=>3)",                "[(1<=>3)]"),
       -- testing combination of negation, disjunction and conjunction
-      ("*(1 +(2 -3))",            "[*(1 +(2 -3))]"), 
+      ("*(1 +(2 -3))",            "[*(1 +(2 -3))]"),
       -- testing invalid Form, missing one bracket ')'  at the end
-      ("*(1 +(2 -3)",             "[]"), 
+      ("*(1 +(2 -3)",             "[]"),
       -- testing whitespaces
-      (" *  (  1 + ( 2 - 3 ) )",  "[*(1 +(2 -3))]"), 
+      (" *  (  1 + ( 2 - 3 ) )",  "[*(1 +(2 -3))]"),
       -- testing combination of negation, disjunction, conjunction and equivalence
-      ("*(2 -3)<==>-*(9 3)",       "[*(2 -3)<==>-*(9 3)]"),
+      ("(*(2 -3)<=>-*(9 3))",       "[(*(2 -3)<=>-*(9 3))]"),
       -- testing combination of negation, disjunction, conjunction and implication
-      ("*(2 -3)==>-+(9 3)",       "[*(2 -3)==>-+(9 3)]")
+      ("(*(2 -3)==>-+(9 3))",       "[(*(2 -3)==>-+(9 3))]")
       ]
 
 compareForm :: String -> String -> Bool
@@ -73,9 +96,9 @@ compareForm a b = show(parse (a)) == b
 
 testParse :: [(String,String)] -> (String -> String -> Bool) -> IO ()
 testParse [] _ = do print ("Done")
-testParse ((s1,s2): pair) f = 
-  if (f s1 s2) 
-    then do 
+testParse ((s1,s2): pair) f =
+  if (f s1 s2)
+    then do
       print ("Test passed on: '" ++ show s1 ++"'")
       testParse pair f
     else do
@@ -90,4 +113,3 @@ numberOfTests = length xs
 numberOfTestsPassed = length ys
 
 allTestsPass = numberOfTests == numberOfTestsPassed
-
