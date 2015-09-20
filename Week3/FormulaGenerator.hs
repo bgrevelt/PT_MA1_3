4457b99126c5d97319d2de3b4b6cc21fd77f7519
jacksierkstra <jacksierkstra@gmail.com>
2015-09-20 17:45:16 +0200

    Added equivalence property.

--- Week3/FormulaGenerator.hs
+++ Week3/FormulaGenerator.hs
@@ -1,14 +1,57 @@
 {-# OPTIONS_GHC -Wall #-}
 module FormulaGenerator where
 
-{-
- I am not going to create a FormulaGenerator. Instead I will make use
- of QuickCheck and come up with some testable properties.
--}
-
 import Test.QuickCheck
 import Lecture3
 import Conversion
+import Control.Monad
+import FormClassification
+
+{-
+  I added a property called isEquivalentAfterConversion, because we want to be sure
+  that there are no modifications on logical end to the formulas. The check goes ok:
+
+  Sample output from the last lines of the tests:
+
+  Passed:
+  (*(0)==>-2)
+  Passed:
+  +(-2 (-1<=>3) +(2))
+  Passed:
+  ((1==>(2==>4))<=>(+(1)==>(-4==>3)))
+  Passed:
+  0
+  Passed:
+  (1==>0)
+  Passed:
+  ((1<=>2)<=>-1)
+  Passed:
+  (2==>+(-2))
+  Passed:
+  0
+  +++ OK, passed 100 tests.
+
+  The other properties are not yet working. My suspicion is that the order of
+  tokens are different from what they are in natural (on paper) written formula's.
+  To give an example:
+
+  The input of convertform could be:
+    ((1==>2)<=>(-2==>-1))
+  And the output will be as follows:
+    *(+(-1 2 1) +(-1 2 -2) +(-1 2 -2) +(-1 2 1) +(2 -1 1) +(2 -1 -2) +(2 -1 -2) +(2 -1 1))
+
+  The properties to test with are testing on the input variant instead of the output.
+
+  Time spent: 12 hours
+
+-}
+
+instance Arbitrary Form where
+ arbitrary = sized form'
+   where
+     form' 0 = liftM Prop arbitrarySizedIntegral
+     form' n | n>0 = oneof [liftM Prop arbitrarySizedIntegral, liftM Neg form'', liftM2 Impl form'' form'', liftM2 Equiv form'' form'', liftM Cnj (replicateM n form''), liftM Dsj (replicateM n form'') ]
+      where form'' = form' (n `div` 2)
 
 isConjunctionOfDisjunctions :: Form -> Bool
 isConjunctionOfDisjunctions f = isConjunctionOfDisjunctions' $ lexer $ show $ convertForm f
@@ -20,7 +63,6 @@ isConjunctionOfDisjunctions' (x:xs) = case x of
                                       TokenInt  {} -> isConjunctionOfDisjunctions' xs
                                       TokenNeg  {} -> case head xs of
                                           TokenInt  {} -> isConjunctionOfDisjunctions' xs
-                                          TokenOP   {} -> isConjunctionOfDisjunctions' xs
                                           _ -> False
                                       TokenCnj  {} -> isConjunctionOfDisjunctions' xs
                                       TokenDsj  {} ->  case head xs of
@@ -30,7 +72,7 @@ isConjunctionOfDisjunctions' (x:xs) = case x of
                                       TokenCP   {} -> case head xs of
                                           TokenCnj {} -> isConjunctionOfDisjunctions' xs
                                           TokenCP  {} -> isConjunctionOfDisjunctions' xs
-                                          _           -> False
+                                          _ -> False
                                       TokenEquiv{} -> False
                                       TokenImpl {} -> False
 
@@ -46,3 +88,11 @@ isDisjunctionOfLiterals' (x:xs) = case x of
                                           TokenDsj {} -> isDisjunctionOfLiterals' xs
                                           _ -> False
                                       _  -> isDisjunctionOfLiterals' xs
+
+-- The following method will check if the formulas are still equivalent after the conversion
+-- to cnf.
+isEquivalentAfterConversion :: Form -> Bool
+isEquivalentAfterConversion f = equivalence f (convertForm f)
+
+testProp :: (Form -> Bool) -> IO()
+testProp f = verboseCheckWith (stdArgs{maxSize=5}) f
