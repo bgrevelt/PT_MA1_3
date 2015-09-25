054eb7e8b23ecd2182d5db85ade35cdd5e16c996
Bouke <b.grevelt@gmail.com>
2015-09-25 22:35:15 +0200

    Added my own version of transitive closure.

--- Week4/Closures.hs
+++ Week4/Closures.hs
@@ -14,8 +14,19 @@ infixr 5 @@
 (@@) :: Eq a => Rel a -> Rel a -> Rel a
 r @@ s = 
   nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
   
 trClos :: Ord a => Rel a -> Rel a
 trClos x | x == rest = x
-         | otherwise = trClos rest 
+         | otherwise = trClos' rest 
          where rest = nub (x ++ (x @@ x))       -- Probably want us to use the operator, would prefer using nub once
+
+{-- 
+My own version. My theory is that this one should be faster than Floris' implementation
+because it composes with the input set, rather than the result of the previous step
+theory is the key word here since in practice Floris' algorithm as faster :'( 
+--}
+trClos' :: Ord a => Rel a -> Rel a
+trClos' i = trClos'' i [] i where
+	trClos'' orig beforeLast last = if beforeLast == last then last else
+		trClos'' orig last (nub (last ++ (last @@ orig)))
