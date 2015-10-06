17bdd81552fed0d76a8c4105e8edea41c992de41
jacksierkstra <jacksierkstra@gmail.com>
2015-10-06 20:15:45 +0200

    Added some test reports along some test methods. I think the testcases speak for themselves

--- Week6/Modular_Exponentation.hs
+++ Week6/Modular_Exponentation.hs
@@ -3,11 +3,79 @@ import Data.Bits
 import Lecture6
 -- https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/fast-modular-exponentiation
 -- https://www.youtube.com/watch?v=EHUgNLN8F1Y
-{-
- Each time, we are going to double and add 1 to the base and the exponential.
--}
-expMTest :: Integer -> Integer
-expMTest e = expM (2) (e*2 + 1) 50
 
-expMTest' :: Integer -> Integer
-expMTest' e = exMOpt (2) (e*2 + 1) 50
+{-
+  First I read some documentation on modular exponentation, then implemented the method in Lecture 6 and
+  then I decided to run some tests. Before these methods, in ghci, I did the following command:
+  :set +s
+  After that, the running time and the memory footprint of the method is shown. That is what I will be showing in
+  this document.
+-}
+
+{-
+
+  Because the method beneath wouldn't run, because m10 already was too big of a footprint, I was
+  profiling with smaller digits.
+
+  *Main> prime_test_F_Unoptimized m7
+  True
+  (0.11 secs, 4,663,464 bytes)
+
+
+-}
+testNaive :: IO ()
+testNaive = do
+            a <- (prime_test_F_Unoptimized m1)
+            b <- (prime_test_F_Unoptimized m2)
+            c <- (prime_test_F_Unoptimized m3)
+            d <- (prime_test_F_Unoptimized m4)
+            e <- (prime_test_F_Unoptimized m5)
+            f <- (prime_test_F_Unoptimized m6)
+            g <- (prime_test_F_Unoptimized m7)
+            h <- (prime_test_F_Unoptimized m8) -- It already stucks at this one..
+            print "Testing for primes beginning with m1 to m8 with the naive algorithm."
+            print ([a,b,c,d,e,f,g,h])
+
+{-
+  Because I want to test whether I had made an optimization, I decided to start where I left off
+  with the naive method. It took way to long with the naive method to even calculate whether m8 is a prime. My
+  new method starts with m8 and goes all the way to m24 and it takes a stunning 7,53 for them all!
+
+  *Main> testOptimized
+  "Testing for primes beginning with m10 to m20 with the optimized algorithm."
+  [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+  (7.53 secs, 725,760,704 bytes)
+
+  *Main> testOptimized
+  "Testing for primes beginning with m10 to m20 with the optimized algorithm."
+  [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+  (7.63 secs, 725,589,416 bytes)
+
+  *Main> testOptimized
+  "Testing for primes beginning with m10 to m20 with the optimized algorithm."
+  [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+  (7.51 secs, 725,589,568 bytes)
+
+-}
+
+testOptimized :: IO ()
+testOptimized = do
+            a <- (prime_test_F m8)
+            b <- (prime_test_F m9)
+            c <- (prime_test_F m10)
+            d <- (prime_test_F m11)
+            e <- (prime_test_F m12)
+            f <- (prime_test_F m13)
+            g <- (prime_test_F m14)
+            h <- (prime_test_F m15)
+            i <- (prime_test_F m16)
+            j <- (prime_test_F m17)
+            k <- (prime_test_F m18)
+            l <- (prime_test_F m19)
+            m <- (prime_test_F m20)
+            n <- (prime_test_F m21)
+            o <- (prime_test_F m22)
+            p <- (prime_test_F m23)
+            q <- (prime_test_F m24)
+            print "Testing for primes beginning with m10 to m20 with the optimized algorithm."
+            print ([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q])
